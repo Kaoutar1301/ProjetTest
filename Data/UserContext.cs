c6336893bdecdd2eef10c8b305778031bb5543aa
Kaoutar1301 <kaoutar1301@gmail.com>
2024-06-18 10:44:53 +0200

    new

---
+++ Data/UserContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using USERPROJECT.Models;
+
+namespace USERPROJECT.Data
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
